e421657c094bb659e36a62ff9dde8c9a889e82c4
Samir Karki <samir.ctec@gmail.com>
2020-03-28 10:26:47 -0600

    use basemodel

--- src/MyHealth.Web/Models/DiseaseSymptom.cs
+++ src/MyHealth.Web/Models/DiseaseSymptom.cs
@@ -3,11 +3,8 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace MyHealth.Web.Models
 {
-    public class DiseaseSymptom
+    public class DiseaseSymptom : BaseModel
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
         public string DiseaseId { get; set; }
         public string SymptomId { get; set; }
         public decimal Score { get; set; }
