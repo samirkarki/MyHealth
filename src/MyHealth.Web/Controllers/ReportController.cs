77310635c82eb2e5ed82d045e86ea9b98a3263db
chandra <technables@gmail.com>
2020-04-10 16:50:42 +0545

    added report controller

---
+++ src/MyHealth.Web/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyHealth.Web.Controllers;
+using MyHealth.Web.Models;
+using MyHealth.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHealth.Web
+{
+    //[Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportController : ControllerBase
+    {
+        private readonly CrudService<UserScore> _userScoreService;
+        public ReportController(CrudService<UserScore> userScoreService)
+        {
+            _userScoreService = userScoreService;
+        }
+
+        [HttpPost]
+        [Route("/report/suspected")]
+        public ActionResult<ReportDTO> DeleteDetails(ReportFilter filter)
+        {
+            var result = _userScoreService.Query(us => us.DiseaseId == filter.DiseaseId).GroupBy(x=>x.CreatedDate).ToList();
+            List<ReportDTO> reports = new List<ReportDTO>();
+
+            return Ok(reports);
+
+        }
+        
+
+    }
+}
---
+++ src/MyHealth.Web/Models/DTO/ReportDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHealth.Web.Models
+{
+    public class ReportDTO
+    {
+        public string x { get; set; }
+        public string y { get; set; }
+    }
+
+    public class ReportFilter
+    {
+        public string DiseaseId { get; set; }
+        public string DateFrom { get; set; }
+        public string DateTo { get; set; }
+    }
+}
